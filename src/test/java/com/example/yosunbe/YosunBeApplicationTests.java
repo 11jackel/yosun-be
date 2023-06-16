4065fd5e73f16b1c82b6655b85bb35e3c6d13bad
11jackel <jackel.shiu@amus.dev>
2023-06-17 03:02:31 +0800

    Initial setting

---
+++ src/test/java/com/example/yosunbe/YosunBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yosunbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YosunBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
